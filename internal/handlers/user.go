6bb7b473ca0bfa31a4d1720ff06a672ecceece90
Ilya Burinsky <burinskiy992@gmail.com>
2024-01-09 16:42:33 +0300

    wip: POST /api/user/login

--- internal/handlers/user.go
+++ internal/handlers/user.go
@@ -3,10 +3,12 @@ package handlers
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/ilya-burinskiy/gophermart/internal/auth"
 	"github.com/ilya-burinskiy/gophermart/internal/services"
 	"github.com/ilya-burinskiy/gophermart/internal/storage"
 )
@@ -22,9 +24,11 @@ func NewUserHandlers(store storage.Storage) UserHandlers {
 func UserRouter(store storage.Storage) chi.Router {
 	handlers := UserHandlers{store: store}
 	registerSrv := services.NewRegisterUserService(store)
+	authenticateSrv := services.NewAuthenticateUserService(store)
 	router := chi.NewRouter()
 	router.Use(middleware.AllowContentType("application/json"))
 	router.Post("/register", handlers.Register(registerSrv))
+	router.Post("/login", handlers.Authenticate(authenticateSrv))
 
 	return router
 }
@@ -52,6 +56,43 @@ func (h UserHandlers) Register(registerSrv services.UserRegistrator) func(http.R
 			var notUniqErr storage.ErrUserNotUniq
 			if errors.As(err, &notUniqErr) {
 				w.WriteHeader((http.StatusConflict))
+				encoder.Encode(err.Error())
+				return
+			}
+			w.WriteHeader(http.StatusInternalServerError)
+			encoder.Encode(err.Error())
+			return
+		}
+
+		setJWTCookie(w, jwtStr)
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func (h UserHandlers) Authenticate(authSrv services.UserAuthenticator) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		type payload struct {
+			Login    string `json:"login"`
+			Password string `json:"password"`
+		}
+		w.Header().Set("Content-Type", "application/json")
+		var requestBody payload
+		decoder := json.NewDecoder(r.Body)
+		encoder := json.NewEncoder(w)
+
+		err := decoder.Decode(&requestBody)
+		if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			encoder.Encode("invalid request body")
+			return
+		}
+
+		jwtStr, err := authSrv.Call(r.Context(), requestBody.Login, requestBody.Password)
+		if err != nil {
+			var notFoundErr storage.ErrUserNotFound
+			fmt.Println(err.Error())
+			if errors.Is(err, auth.ErrInvalidCreds) || errors.As(err, &notFoundErr) {
+				w.WriteHeader(http.StatusUnauthorized)
 				encoder.Encode(err.Error())
 				return
 			}
