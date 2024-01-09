045ced1e86a9bd349466062ed425d31c0244382c
Ilya Burinsky <burinskiy992@gmail.com>
2024-01-09 18:11:46 +0300

    logging middleware

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -3,8 +3,11 @@ package main
 import (
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/ilya-burinskiy/gophermart/internal/handlers"
+	"github.com/ilya-burinskiy/gophermart/internal/middlewares"
 	"github.com/ilya-burinskiy/gophermart/internal/storage"
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -13,11 +16,33 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	router := handlers.UserRouter(db)
+	logger := configureLogger("info")
+
+	router := chi.NewRouter()
+	router.Use(
+		middlewares.LogResponse(logger),
+		middlewares.LogRequest(logger),
+	)
+	router.Mount("/api/user", handlers.UserRouter(db))
 
 	server := http.Server{
 		Handler: router,
 		Addr:    "localhost:8080",
 	}
 	server.ListenAndServe()
+}
+
+func configureLogger(level string) *zap.Logger {
+	logLvl, err := zap.ParseAtomicLevel(level)
+	if err != nil {
+		panic(err)
+	}
+	loggerConfig := zap.NewProductionConfig()
+	loggerConfig.Level = logLvl
+	logger, err := loggerConfig.Build()
+	if err != nil {
+		panic(err)
+	}
+
+	return logger
 }
