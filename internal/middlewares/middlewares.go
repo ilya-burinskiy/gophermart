10f53a90149c0de7202bef18811f6870e37ac9a2
Ilya Burinsky <burinskiy992@gmail.com>
2024-01-11 16:16:21 +0300

    auth middleware

--- internal/middlewares/middlewares.go
+++ internal/middlewares/middlewares.go
@@ -1,10 +1,13 @@
 package middlewares
 
 import (
+	"context"
 	"net/http"
 	"strings"
 	"time"
 
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/ilya-burinskiy/gophermart/internal/auth"
 	"github.com/ilya-burinskiy/gophermart/internal/compress"
 	"go.uber.org/zap"
 )
@@ -14,6 +17,10 @@ type loggingResponseWriter struct {
 	Status int
 	Size   int
 }
+
+type contextKey string
+
+const userIDKey contextKey = "user_id"
 
 func (lw *loggingResponseWriter) Write(bytes []byte) (int, error) {
 	size, err := lw.ResponseWriter.Write(bytes)
@@ -78,4 +85,31 @@ func GzipCompress(h http.Handler) http.Handler {
 
 		h.ServeHTTP(w, r)
 	})
+}
+
+func Authenticate(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cookie, err := r.Cookie("jwt")
+		if err != nil {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		claims := &auth.Claims{}
+		token, err := jwt.ParseWithClaims(cookie.Value, claims, func(token *jwt.Token) (interface{}, error) {
+			return []byte(auth.SecretKey), nil
+		})
+		if err != nil || !token.Valid {
+			w.WriteHeader(http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), userIDKey, claims.UserID)
+		h.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+func UserIDFromContext(ctx context.Context) (int, bool) {
+	userID, ok := ctx.Value(userIDKey).(int)
+	return userID, ok
 }
