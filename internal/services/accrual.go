4d95ceb7d444ca469ae7cf2c6bd9a66383a658d1
Ilya Burinsky <burinskiy992@gmail.com>
2024-01-16 11:04:08 +0300

    wip: AccrualWorker

---
+++ internal/services/accrual.go
@@ -0,0 +1,100 @@
+package services
+
+import (
+	"context"
+	"errors"
+
+	"github.com/ilya-burinskiy/gophermart/internal/accrual"
+	"github.com/ilya-burinskiy/gophermart/internal/models"
+	"github.com/ilya-burinskiy/gophermart/internal/storage"
+	"go.uber.org/zap"
+)
+
+type AccrualWorker interface {
+	Run()
+	Register(order models.Order)
+}
+
+type accrualWorker struct {
+	client  accrual.ApiClient
+	store   storage.Storage
+	logger  *zap.Logger
+	channel chan models.Order
+}
+
+// TODO: accept exitCh for graceful shutdown
+func NewAccrualWorker(
+	accrualApiClient accrual.ApiClient,
+	store storage.Storage,
+	logger *zap.Logger) AccrualWorker {
+
+	return accrualWorker{
+		client:  accrualApiClient,
+		store:   store,
+		logger:  logger,
+		channel: make(chan models.Order),
+	}
+}
+
+func (wrk accrualWorker) Register(order models.Order) {
+	wrk.channel <- order
+}
+
+func (wrk accrualWorker) Run() {
+	for {
+		order := <-wrk.channel
+		orderInfo, err := wrk.client.GetOrderInfo(context.TODO(), order.Number)
+		if err != nil {
+			wrk.logger.Info(
+				"failed to get order info",
+				zap.String("order_number", order.Number),
+				zap.Error(err),
+			)
+			continue
+		}
+
+		ctx := context.TODO()
+		wrk.updateOrderWithBalance(ctx, order, orderInfo)
+	}
+}
+
+func (wrk accrualWorker) updateOrderWithBalance(ctx context.Context, order models.Order, orderInfo accrual.OrderInfo) {
+	tx, err := wrk.store.BeginTranscaction(ctx)
+	if err != nil {
+		wrk.logger.Info("failed to start transaction", zap.Error(err))
+		return
+	}
+	defer tx.Rollback(ctx)
+
+	err = wrk.store.UpdateOrder(ctx, order.ID, orderInfo.Status, orderInfo.Accrual)
+	if err != nil {
+		wrk.logger.Info("failed to update order", zap.Error(err))
+		return
+	}
+
+	balance, err := wrk.store.FindBalanceByUserID(ctx, order.UserID)
+	if err != nil {
+		var notFoundErr storage.ErrBalanceNotFound
+		if errors.As(err, &notFoundErr) {
+			balance, err = wrk.store.CreateBalance(ctx, order.UserID, orderInfo.Accrual)
+			if err != nil {
+				wrk.logger.Info("failed to create balance", zap.Error(err))
+				return
+			}
+		}
+
+		wrk.logger.Info("an unexpted error occured while trying to find balance", zap.Error(err))
+		return
+	}
+
+	err = wrk.store.UpdateBalanceCurrentAmount(ctx, balance.ID, balance.CurrentAmount+orderInfo.Accrual)
+	if err != nil {
+		wrk.logger.Info("failed to updage balance current amount", zap.Error(err))
+		return
+	}
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		wrk.logger.Info("failed to commit transaction", zap.Error(err))
+	}
+}
---
+++ internal/services/accrual_test.go
@@ -0,0 +1,71 @@
+package services_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/ilya-burinskiy/gophermart/internal/accrual"
+	"github.com/ilya-burinskiy/gophermart/internal/models"
+	"github.com/ilya-burinskiy/gophermart/internal/services"
+	"github.com/ilya-burinskiy/gophermart/internal/storage"
+	"github.com/ilya-burinskiy/gophermart/internal/storage/mocks"
+	"github.com/stretchr/testify/mock"
+	"go.uber.org/zap/zaptest"
+)
+
+type accrualApiClientMock struct{ mock.Mock }
+
+func (m *accrualApiClientMock) GetOrderInfo(ctx context.Context, orderNumber string) (accrual.OrderInfo, error) {
+	call := m.Called(ctx, orderNumber)
+	return call.Get(0).(accrual.OrderInfo), call.Error(1)
+}
+
+func TestAccrualRun(t *testing.T) {
+	orderInfo := accrual.OrderInfo{Number: "123", Status: models.ProcessedOrder, Accrual: 10}
+	order := models.Order{ID: 1, UserID: 1, Number: "123"}
+	balance := models.Balance{ID: 1, UserID: 1}
+
+	logger := zaptest.NewLogger(t)
+	accrualApiClientMock := new(accrualApiClientMock)
+	accrualApiClientMock.
+		On("GetOrderInfo", mock.Anything, "123").
+		Return(orderInfo, nil)
+
+	t.Run("it updates order and balance current amount", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		txMock := mocks.NewMockTx(ctrl)
+		txMock.EXPECT().Commit(gomock.Any()).AnyTimes().Return(nil)
+		txMock.EXPECT().Rollback(gomock.Any()).AnyTimes().Return(nil)
+
+		storageMock := mocks.NewMockStorage(ctrl)
+		storageMock.EXPECT().BeginTranscaction(gomock.Any()).Return(txMock, nil)
+		storageMock.EXPECT().UpdateOrder(gomock.Any(), order.ID, orderInfo.Status, orderInfo.Accrual).Return(nil)
+		storageMock.EXPECT().FindBalanceByUserID(gomock.Any(), order.ID).Return(balance, nil)
+		storageMock.EXPECT().
+			UpdateBalanceCurrentAmount(gomock.Any(), balance.ID, balance.CurrentAmount+orderInfo.Accrual).Return(nil)
+
+		accrualWrk := services.NewAccrualWorker(accrualApiClientMock, storageMock, logger)
+		go accrualWrk.Run()
+		accrualWrk.Register(order)
+	})
+
+	t.Run("it creates balance and updates order", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		txMock := mocks.NewMockTx(ctrl)
+		txMock.EXPECT().Commit(gomock.Any()).AnyTimes().Return(nil)
+		txMock.EXPECT().Rollback(gomock.Any()).AnyTimes().Return(nil)
+
+		storageMock := mocks.NewMockStorage(ctrl)
+		storageMock.EXPECT().BeginTranscaction(gomock.Any()).Return(txMock, nil)
+		storageMock.EXPECT().UpdateOrder(gomock.Any(), order.ID, orderInfo.Status, orderInfo.Accrual).Return(nil)
+		storageMock.EXPECT().
+			FindBalanceByUserID(gomock.Any(), order.UserID).Return(models.Balance{}, storage.ErrBalanceNotFound{Balance: models.Balance{UserID: order.UserID}})
+		storageMock.EXPECT().
+			CreateBalance(gomock.Any(), order.UserID, orderInfo.Accrual).Return(models.Balance{ID: 1, UserID: order.UserID, CurrentAmount: orderInfo.Accrual}, nil)
+
+		accrualWrk := services.NewAccrualWorker(accrualApiClientMock, storageMock, logger)
+		go accrualWrk.Run()
+		accrualWrk.Register(order)
+	})
+}
